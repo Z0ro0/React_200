83fcb9c9a3752b7bb0bf5a57da0622a5a5360bdf
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-18 22:57:41 +0900

    생명주기 함수 render() 함수-return 되는 html 형식의 코드를 화면에 그려줌

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import ImportConponent from './R003_ImportComponent';
+import LifecycleEx from './R004_LifecycleEx';
 
 function App() {
   return (
     <div>
       <h1>start React200</h1>
       <p>HTML 적용하기</p>
-      <ImportConponent></ImportConponent>
+      <LifecycleEx></LifecycleEx>
     </div>
   );
 }
