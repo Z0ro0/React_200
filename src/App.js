39f0a51fbe8b2e63c2a1ec80cff3e12fba7e50db
Z0ro0 <chlrkdmf0805@gmail.com>
2023-05-18 22:50:21 +0900

    Component 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ImportConponent from './R003_ImportComponent';
 
 function App() {
   return (
     <div>
       <h1>start React200</h1>
       <p>HTML 적용하기</p>
+      <ImportConponent></ImportConponent>
     </div>
   );
 }
---
+++ src/R003_ImportComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class R003_ImportComponent extends Component{
+    render(){
+        return(
+            <h2>[This is Component]</h2>
+        )
+    }
+}
+
+export default R003_ImportComponent;
